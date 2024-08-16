b7f7864d8da5e298c3aa94a3b701c67a0bc21ff1
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2024-08-16 22:37:34 +0530

    SLL files

---
+++ Delete at end.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}nd;
+nd *createlist(nd *,int);
+void display(nd *);
+int main()
+{
+    int ch,n,j;
+    nd *head,*node,*head1;
+    printf("Enter the no. of nodes in linked list: ");
+    scanf("%d",&n);
+    head=(nd *)malloc(sizeof(nd));
+    head->next=NULL;
+    head=createlist(head,n);
+    printf("Before deletion:\n");
+    display(head);
+    head1=head;
+    while(head1->next->next!=NULL)
+    {
+        head1=head1->next;
+    }
+    head1->next=NULL;
+    printf("After deletion:\n");
+    display(head);
+    return 0;
+}
+nd* createlist(nd *head1,int n)
+{
+    int ch=1;
+    nd *node,*head;
+    head=head1;
+    printf("Enter the first data: ");
+    scanf("%d",&head1->data);
+    while(ch<n)
+    {
+        node=(nd *)malloc(sizeof(nd));
+        printf("Enter the next data: ");
+        scanf("%d",&node->data);
+        head->next=node;
+        node->next=NULL;
+        head=head->next;
+        ch++;
+    }
+    return head1;
+}
+void display(nd *head)
+{
+    if(head->next==NULL)
+    {
+        printf("There is no element in the Linked List\n");
+        return ;
+    }
+    printf("The Elements In the Linked List are:\n ");
+    while(head!=NULL) 
+    {
+        printf("%d -> ",head->data);
+        head=head->next;
+    }
+    printf("NULL\n");
+}
